37045524cec2bffd8dd68d837578838574617f1e
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-04-29 18:22:02 +0200

    Fin operaciones

---
+++ bloque1/02operaciones.js
@@ -0,0 +1,28 @@
+// asignacion
+var a = 1;
+var b = 2;
+
+// suma
+var suma = a + b;
+// resta
+var resta =  a - b;
+// multiplicacion
+var mult = a * b;
+// division 
+var divi = a / b;
+// modulo o resto
+var resto = a % b;
+// exponenciacion
+var expo = a ** b;
+
+// incremento
+a = a + 1;
+a++;
+a += 2;
+
+// decremento
+a = a - 1;
+a--;
+a -= 2;
+
+console.log(a);
