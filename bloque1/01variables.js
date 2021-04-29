c0640973d2cc2e7cf3a4c66db84afcdd6e62760a
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-04-29 17:57:49 +0200

    Fin variables, empieza operaciones

--- bloque1/01variables.js
+++ bloque1/01variables.js
@@ -1,3 +1,19 @@
 var cajaA = 3;
+cajaA = 5;
 var cajaB = 5;
-var cajaC = 8;
+var cajaC = cajaA + cajaB;
+
+console.log(cajaA, cajaB, cajaC);
+
+var x = 2;
+var y = 3;
+
+//intercambiar los valores de x e y
+//x:2, y:3
+//resultado: x:3, y:2
+
+var z = x;
+x = y;
+y = z;
+
+console.log(x, y)
