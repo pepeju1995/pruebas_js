c8c1326a9da5a8ebdb7fedc3786d7bc883ef5a5b
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-04-30 07:56:02 +0200

    Fin de tipos de datos

---
+++ bloque1/04tiposdatos.js
@@ -0,0 +1,22 @@
+//Que son los tipos
+
+// Los tipos de javascript:
+var numero = 2.5;
+var string = 'Soy un texto';
+var booleano = false;
+
+var array = ["aa", "bb", 3, [1, 2]];
+var objeto = {
+    nombre: 'Jose Juan',
+    apellido: 'Perez Gonzalez',
+    edad: 26
+}
+
+var funcion = function() {}
+
+// los tipos son dinamicos
+var a = 4;
+a = 'Hello';
+
+// typeof
+console.log(typeof booleano)
