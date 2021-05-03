156d15b919f773db74a1126131f4a89514ee8edd
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-03 16:58:53 +0200

    Fin ifelse

---
+++ bloque1/06ifelse.js
@@ -0,0 +1,10 @@
+// control de flujo
+
+var primer = 25;
+var segundo = 2;
+
+if (primer % segundo == 0) {
+    console.log(primer + " es multiplo de " + segundo);
+} else {
+    console.log(primer + " no es multiplo de " + segundo);
+}
