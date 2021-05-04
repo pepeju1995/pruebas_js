a32c9e2185dd690d247039a910a886a60d8798ba
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-04 16:16:37 +0200

    Inicio bucles

---
+++ bloque1/08bucles.js
@@ -0,0 +1,20 @@
+// Bucle while
+var contador = 0;
+var N = 50;
+while (contador < N) {
+    console.log(contador);
+    contador ++;
+}
+
+/*
+// Bucle do while
+do {
+
+} while (condicion);
+*/
+/*
+// Bucle for
+for (let i = 0; i < 10; i++) {
+
+}
+*/
