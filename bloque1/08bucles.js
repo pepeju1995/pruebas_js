f4dbe4a594b7a8089458a634271adf0d8ac23302
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-05 08:16:29 +0200

    Fin bucles

--- bloque1/08bucles.js
+++ bloque1/08bucles.js
@@ -1,11 +1,11 @@
-// Bucle while
+/*// Bucle while
 var contador = 0;
 var N = 50;
 while (contador < N) {
     console.log(contador);
     contador ++;
 }
-
+*/
 /*
 // Bucle do while
 do {
@@ -14,7 +14,44 @@ do {
 */
 /*
 // Bucle for
-for (let i = 0; i < 10; i++) {
-
+for (let i = 0; i < 10; ++i) {
+    console.log(i);
 }
 */
+/*
+var array = [10 , 20, 30];
+var estudiantes = [
+    {
+        nombre: "Estudiante 1",
+        nota: 5
+    },
+    {
+        nombre: "Estudiante 2",
+        nota: 8
+    },
+    {
+        nombre: "Estudiante 3",
+        nota: 1
+    }
+]
+// Iterar sobre vectores y objetos
+for (let item of estudiantes) {
+    console.log(item.nombre, item.nota);
+}
+
+var estudianteAuxiliar = {
+    nombre: "Estudiante 1",
+    nota: 5
+}
+for (let key in estudianteAuxiliar) {
+    console.log(estudianteAuxiliar[key]);
+}
+*/
+
+// Arbol de navidad
+var altura = 7
+
+for (let i = 1; i <= altura; i++) {
+    console.log(" ".repeat(altura - i), "*".repeat(i * 2 - 1));
+    //console.log("*".repeat(i * 2 - 1));    
+}
