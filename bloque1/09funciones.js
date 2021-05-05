c7c45c2e4605fab9ff34fef602e690a74abc7898
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-05 08:57:13 +0200

    Fin funciones

--- bloque1/09funciones.js
+++ bloque1/09funciones.js
@@ -8,8 +8,8 @@ function proceso1() {
 proceso1();
 // Repetidamente
 proceso1();
-
-
+*/
+/*
 // Les podemos enviar datos
 
 function generateFullName(nombre, apellido) {
@@ -18,7 +18,7 @@ function generateFullName(nombre, apellido) {
 
 generateFullName("Jose Juan", "Perez Gonzalez");
 */
-
+/*
 // Podemos hacer que nos devuelvan datos
 function calculateMidPoint(num1, num2) {
     return (num1 + num2)/2;
@@ -28,3 +28,37 @@ var firstNumber = 2;
 var secondNumber = 4;
 var midPoint = calculateMidPoint(firstNumber, secondNumber);
 console.log("La media de " + firstNumber + " y " + secondNumber + " es: " + midPoint);
+*/
+/*
+// Las funciones son de tipo de dato, por lo que podemos utilizarlo como variable
+
+var logicaSaludar = function(nombre) {console.log("Hola mi nombre es " + nombre)};
+var logicaSaludarMaleducado = function(nombre) {console.log("Hola, no me gustas pero mi nombre es " + nombre)};
+
+// En logicaSaludar teenemos la referencia a la funcion
+// Podeemos hacer algo tan loco como pasar una funcion a otra
+function saludarSuperEducadamente(funcionPasadaPorParametro, nombre) {
+    console.log("Disculpee, perdone que le moleste, pero ");
+    // Ejecutamos la funcion que hemos recibido
+    funcionPasadaPorParametro(nombre);
+    console.log("Espero pasar una muy buena velada con usted.");
+}
+
+// Lo ejecutamos todo
+saludarSuperEducadamente(logicaSaludarMaleducado, "Jose Juan");
+
+
+*/
+
+function multiplicarVectorPorNumero(vectorNumeros, numero){
+    var nuevoVector = [];
+    for (let num of vectorNumeros) {
+        nuevoVector.push(num * numero);
+    }
+    return nuevoVector;
+}
+
+var vector = [25, 50, 30];
+var numero = 3;
+
+console.log(multiplicarVectorPorNumero(vector, numero));
